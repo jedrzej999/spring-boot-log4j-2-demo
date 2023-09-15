e50d1e647962c18fe46e7b91cfd272f534c4f16f
Jędrzej Szafałowicz <jedrzej.szafalowicz@paypo.pl>
2023-09-15 15:25:56 +0200

    Example with separate MDC and NDC usage

--- src/main/java/com/example/log4j2demo/Log4j2DemoApplication.java
+++ src/main/java/com/example/log4j2demo/Log4j2DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.log4j2demo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,18 +10,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Log4j2DemoApplication implements ApplicationRunner {
-	private static final Logger logger = LogManager.getLogger(Log4j2DemoApplication.class);
+	private static final Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Log4j2DemoApplication.class, args);
 	}
 
 	@Override
-	public void run(ApplicationArguments applicationArguments) throws Exception {
+	public void run(ApplicationArguments applicationArguments) {
+		ThreadContext.put("Debugging", "Data");
+		ThreadContext.push("NDC Data");
 		logger.debug("Debugging log");
+		ThreadContext.clearAll();
+
+		ThreadContext.put("Info", "Data");
+		ThreadContext.push("Info log NDC Data");
 		logger.info("Info log");
+		ThreadContext.clearAll();
+
+		ThreadContext.put("Warn", "Data");
+		ThreadContext.push("Warning NDC Data");
 		logger.warn("Hey, This is a warning!");
+		ThreadContext.clearAll();
+
+		ThreadContext.push("This will be appended to both error and fatal logs");
 		logger.error("Oops! We have an Error. OK");
 		logger.fatal("Damn! Fatal error. Please fix me.");
+		ThreadContext.clearAll();
 	}
 }
